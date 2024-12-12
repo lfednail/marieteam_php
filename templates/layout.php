ecf3637afb36e5371898c7380cb8840eda0ba661
lfednail <lfednail@gmail.com>
2024-12-12 10:09:29 +0100

    update layout css

--- templates/layout.php
+++ templates/layout.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["title"] /* est defini dans le routage */ ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <nav>
+<body class="bg-gray-50 text-gray-800">
+    <nav class="bg-blue-800 text-white shadow-md">
+        <div class="container mx-auto flex items-center justify-between w-full"
         <ul>
             <il><a href="/marieteam_php/public">Acceuil</a></il>
             <il><a href="liaisons">les liaisons</a></il>
