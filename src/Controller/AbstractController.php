82147ea2e117b6ae5598e8d2da5892c1ab57ec05
etan.macret <et.macret@gmail.com>
2024-12-12 09:25:33 +0100

    correction struct routage

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+abstract class AbstractController {
+
+	protected function renderView(string $template, array $data = []): string {
+		$templatePath = dirname(__DIR__, 2) . '/templates/' . $template;
+		return require_once dirname(__DIR__, 2) . '/templates/layout.php';
+	}
+
+    protected function redirectToRoute(string $path, array $params = [])
+    {
+		$uri = $_SERVER['SCRIPT_NAME'] . "?path=" . $path;
+
+		if (!empty($params)) {
+			$strParams = [];
+			foreach ($params as $key => $val) {
+				array_push($strParams, urlencode((string) $key) . '=' . urlencode((string) $val));
+			}
+			$uri .= '&' . implode('&', $strParams);
+		}
+
+		header("Location: " . $uri);
+		die;
+	}
+
+}
