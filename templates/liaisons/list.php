882e956c4c9298c043f0f5bbb38b1e5b28e10522
NathanVALLES <nathanvalles0504@gmail.com>
2025-01-16 10:26:44 +0100

    style update

--- templates/liaisons/list.php
+++ templates/liaisons/list.php
@@ -5,13 +5,13 @@ $listLiaisons = $db->select("select * from liaison");
 ?>
 
 <div class="container mx-auto max-w-lg p-8 bg-white shadow-lg rounded space-y-5 my-10 ">
-    <h2 class="text-3xl font-bold text-center mb-6">Crossings</h2>
+    <h2 class="text-3xl font-bold text-center mb-6">Liaisons</h2>
     <?php foreach ($listLiaisons as $liaison):?>
 
         <div class="bg-white shadow-md rounded-lg overflow-hidden p-4">
 
             <h5>
-                <a href=<?= "liaisons/" . $liaison["id_Liaison"] ?> >
+                <a class="text-blue-600 font-bold" href=<?= "liaisons/" . $liaison["id_Liaison"] ?> >
                     <?= $liaison["Lieu_depart"] . ' - ' . $liaison["Lieu_arrivee"] ?>
                 </a>
             </h5>
