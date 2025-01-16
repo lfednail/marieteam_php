e70b3f5d9bb053ee8577a4d3a59f19ed47081a17
etan.macret <et.macret@gmail.com>
2025-01-16 12:17:47 +0100

    creation page boat

--- templates/main/boat.php
+++ templates/main/boat.php
@@ -4,9 +4,59 @@ use App\DB\BDD;
 
 $db = new BDD();
 
-if(isset($data['id'])){
-    require dirname(__DIR__) . "/boat/detail.php";
-}else{
-    require dirname(__DIR__) . "/boat/list.php";
-}
+$boatCruse = $db->select("select * from viewvoyageur");
+$boatFret = $db->select("select * from viewfret");
 
+?>
+
+<!-- to the left -->
+<div>
+    <h3>Cruses</h3>
+
+    <?php foreach ($boatCruse as $cruse):?>
+        <?php $picture = $db->selectOne("Select * from image where id_Image = {$cruse['id_Image']}") ?>
+        <?php $listEquipement = $db->select("Select * from viewliste_equipement wher id_Bateau = {$cruse['id_Bateau']}") ?>
+        <div class="p-5">
+            
+            <img src="/marieteam_php/public/assets/<?= $picture['Nom_image'] . "." . $picture['Extension'] ?>" alt="image du bateau">
+            <h5><?= $cruse['Nom_bateau']?></h5>
+            <br>
+
+            <p>Length: <?= $cruse['Longueur_bateau'] ?></p>
+            <br>
+
+            <p>Width: <?= $cruse['Largeur_bateau'] ?></p>
+            <br>
+
+            <p>Speed: <?= $cruse['Vitesse'] ?></p>
+            <br>
+
+            <p>Number of seats: <?= $cruse['Places'] ?></p>
+            <br>
+
+            <p>Equipement</p>
+            <p>
+                <?php foreach ($listEquipement as $e):?>
+                    <?= $e['Lib_equipement'] . ': ' . $e['Quantite_equipement'] ?><br>
+                <?php endforeach; ?>
+            </p>
+            <br>
+
+        </div>
+    <?php endforeach; ?>
+
+</div>
+
+<!-- to the right -->
+<div>
+    <h3>Fret</h3>
+
+    <?php foreach ($boatFret as $fret):?>
+        <div class="p-5">
+
+            <h5><?= $fret['Nom_bateau']?></h5>
+
+        </div>
+    <?php endforeach; ?>
+
+</div>
