fe80a59d857daffb0418163e6ad0f1e6a9afbe47
lfednail <lfednail@gmail.com>
2025-02-06 09:17:06 +0100

    update viewRegister with checkbox show password

--- vue/viewRegister.php
+++ vue/viewRegister.php
@@ -27,9 +27,19 @@ if(!empty($errors)){
         <div class="mb-6">
             <label for="password" class="block text-gray-700">Password</label>
             <input type="password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-md" required>
+            <div class="mt-4">
+                <input type="checkbox" id="show-password" class="mr-2">
+                <label for="show-password" class="text-gray-700 text-sm">Show Password</label>
+            </div>
         </div>
         <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700">Register</button>
     </form>
     <p class = "py-2 text-sm">Do you already have an account? Use it!</p>
     <button type="button" onclick=location.href="login" class="w-40 bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700">Connection</button>
 </div>
+<script>
+    document.getElementById('show-password').addEventListener('change', function () {
+        const passwordField = document.getElementById('password');
+        passwordField.type = this.checked ? 'text' : 'password';
+    });
+</script>
